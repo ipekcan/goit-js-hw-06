40cf30f176e4713b3ee4d47f35aad0eb8b5d75aa
ipekcan <ipekcan@hotmail.com>
2024-12-06 11:30:09 +0300

    task-2 completed

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,23 @@
 "use strict";
+class Storage {
+    #items;
+    constructor(arr) {
+        this.#items = arr;
+    }
+    getItems() {
+        return this.#items;
+    }
+    addItem(newItem){
+        this.#items.push(newItem);
+    }
+    removeItem(itemToRemove) {
+        const newArr = this.#items.filter(i => i!== itemToRemove);
+        this.#items = newArr;
+    }
+}
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
