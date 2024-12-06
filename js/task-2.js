f1325798a5cdfe57146e1d5205127d700f99e651
ipekcan <ipekcan@hotmail.com>
2024-12-06 11:48:41 +0300

    prettier

--- js/task-2.js
+++ js/task-2.js
@@ -1,19 +1,19 @@
 "use strict";
 class Storage {
-    #items;
-    constructor(arr) {
-        this.#items = arr;
-    }
-    getItems() {
-        return this.#items;
-    }
-    addItem(newItem){
-        this.#items.push(newItem);
-    }
-    removeItem(itemToRemove) {
-        const newArr = this.#items.filter(i => i!== itemToRemove);
-        this.#items = newArr;
-    }
+  #items;
+  constructor(arr) {
+    this.#items = arr;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const newArr = this.#items.filter((i) => i !== itemToRemove);
+    this.#items = newArr;
+  }
 }
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,21 @@
 "use strict";
 class StringBuilder {
-    #value;
-    constructor(initialValue) {
-        this.#value = initialValue;
-    }
-    getValue() {
-        return this.#value;
-    }
-    padEnd(str) {
-        this.#value = this.#value+str;
-    }
-    padStart(str) {
-        this.#value = str+this.#value;
-    }
-    padBoth(str) {
-        this.#value = str+this.#value+str;
-    }
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value = this.#value + str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
 }
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
